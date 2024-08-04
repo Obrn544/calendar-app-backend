ec9f16604cb0fad5d2ffb3f3360fee9fdfd47019
Omar Obregon <oobregon544@gmail.com>
2024-08-03 19:10:40 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+// public directory
+app.use(express.static('public'));
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+
+// Listening requests
+app.listen(process.env.PORT, () => {
+    console.log('server running in port 4000');
+});
